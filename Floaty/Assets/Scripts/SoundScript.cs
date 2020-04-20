51a4b63f300c5633cefa8ea5bf399a0c72f45558
AccountForF <63286904+AccountForF@users.noreply.github.com>
2020-04-20 15:50:13 +0800

    BGM, ItemSound and PumpSound

---
+++ Floaty/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour
+{
+    public AudioSource myAudio;
+    public AudioClip myBGM, myPumpClip, myItemClip;
+
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myBGM = Resources.Load<AudioClip>("BGM");
+        myPumpClip = Resources.Load<AudioClip>("PumpClip");
+        myItemClip = Resources.Load<AudioClip>("ItemClip");
+        myAudio.PlayOneShot(myBGM);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            myAudio.PlayOneShot(myPumpClip);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Item"|| other.gameObject.name == "Item (1)" || other.gameObject.name == "Item (2)" || other.gameObject.name == "Item (3)" || other.gameObject.name == "Item (4)")
+        {
+            myAudio.PlayOneShot(myItemClip);
+        }
+    }
+}
